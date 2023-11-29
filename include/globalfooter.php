98b7ced1616c6ed74397125b166ec4e193fc55f2
muskan dhingra <146599472+MUSKAN-DHINGRA@users.noreply.github.com>
2023-11-29 15:24:07 -0500

    Initial Commit - Committing files.

---
+++ include/globalfooter.php
@@ -0,0 +1,4 @@
+<link rel = "stylesheet" href="css/style.css" type="text/css">
+
+
+<p>@2023 Employee Portal All rights reserved.</p>
---
+++ include/globalheader.php
@@ -0,0 +1,19 @@
+<link rel="stylesheet" href="../css/style.css" type="text/css">
+
+<nav class="navbar navbar-dark bg-primary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="index.php"><img src="images/Employee-Portal-Logo.png" alt="header logo"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="display-person.php">View</a></li>
+            <li class="nav-item"><a class="nav-link" href="signin.php">Sign In</a></li>
+            <li class="nav-item"><a class="nav-link" href="addimage.php">ADD Images</a></li>
+            <li class="nav-item"><a class="nav-link" href="viewimage.php">View Images</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
